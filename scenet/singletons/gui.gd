4c4a8fd8c63e8ec8d69cc789a0c767c69542fe30
Diizisti <89035273+Diizisti@users.noreply.github.com>
2021-10-20 20:13:23 +0300

    gui
    +timer
    +deathcount (wip)

---
+++ scenet/singletons/gui.gd
@@ -0,0 +1,50 @@
+extends Control
+
+# Ajastin
+var ms := 0
+var s  := 0
+var m  := 0
+
+func _ready():
+	visible = false
+
+func start_timer():
+	visible = true
+	
+	# Resettaa ajastin
+	ms = 0
+	s  = 0
+	m  = 0
+
+func stop_timer():
+	visible = false
+
+func get_time():
+	return "Timer: %s:%s.%s" % [m, str(s).pad_zeros(2), str(ms).pad_zeros(2)]
+
+func update_timer(delta:float):
+	# Älä päivitä ajastinta jos ei ole näkyvillä.
+	if !visible:
+		return
+	
+	# Lisää millisekunnit
+	ms += (delta * 100.0)
+	
+	# Jos yli 1000ms niin lisää 1 sekuntteihin, ja miinusta 1000 millisekunneista.
+	if ms >= 100:
+		ms -= 100
+		s += 1
+	# Jos sekunnit yli 60 niin lisää 1 minuutteihin ja miinusta 60 sekunttia.
+	if s >= 60:
+		s -= 60
+		m += 1
+	
+	# Päivitä ajastimen teksti.
+	$timer.text = get_time()
+
+func update_deathcount():
+	$death_count.text = "Deaths: %s" % [Globals.death_count]
+
+func _physics_process(delta:float):
+	update_timer(delta)
+	update_deathcount()
