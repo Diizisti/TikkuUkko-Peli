93b9298bcfe3ebdc78ea083e7b234bbf3f2d5b16
Diizisti <89035273+Diizisti@users.noreply.github.com>
2021-10-21 18:30:02 +0300

    partikkeleita portaaliin.

--- palikat/portal.gd
+++ palikat/portal.gd
@@ -4,5 +4,11 @@ extends Area2D
 export(String) var next_level_path
 
 func _on_portal_body_entered(body):
-	if body is Player: # Jos pelaaja osu portaaliin niin vaihda kenttää
+	if body is Player: # Jos pelaaja osu portaaliin 
+		# Käynnistä partikkelit ja piilota pelaaja.
+		$finished_particles.emitting = true
+		body.visible = false
+		
+		# Odota 1 sekuntti ja vaihda kenttää, "finished_particles" kestää yhen sekunnin.
+		yield(get_tree().create_timer(1.0), "timeout")
 		get_tree().change_scene(next_level_path)
