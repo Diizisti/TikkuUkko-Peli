c216105987a31313fad687d94060fe7a15f563c3
Diizisti <89035273+Diizisti@users.noreply.github.com>
2021-10-22 23:36:05 +0300

    Veri partikkelit

--- pelaaja/pelaaja.gd
+++ pelaaja/pelaaja.gd
@@ -1,13 +1,15 @@
 extends KinematicBody2D
 class_name Player
 
+const BLOOD_PARTICLE = preload("res://pelaaja/veri.tscn")
 const GRAVITY       = 45
 const JUMP_STRENGTH = 720
 const MOVE_SPEED    = 420
 const CROUCH_SPEED  = 220
 const MAX_JUMPS     = 2 # 1 in ground and 1 in air
 
-var motion := Vector2()
+onready var spawn_pos = global_position # Tallenna alotus positio respawnausta varten kun kuolee.
+var motion  = Vector2()
 var is_crouching = false
 var jumps_left = MAX_JUMPS # hyppääminen miinustaa ja maahan koskeminen laitaa takas kahdeksi, tupla hyppy.
 
@@ -67,6 +69,12 @@ func _physics_process(delta):
 		motion.y = -JUMP_STRENGTH
 	# Liiku
 	motion = move_and_slide(motion, Vector2.UP)
+	
+	# Takaisin alkuruutuun "ESC":istä
+	if Input.is_action_just_pressed("ui_cancel"):
+		Gui.stop_timer()
+		get_tree().change_scene("res://scenet/alotusruutu.tscn")
+
 
 # kill the player
 func die():
@@ -74,4 +82,13 @@ func die():
 	if !visible:
 		return
 	Globals.death_count += 1
-	get_tree().reload_current_scene()
+	
+	# Veri partikkelit
+	var instance = BLOOD_PARTICLE.instance()
+	add_child(instance)
+	instance.set_as_toplevel(true)
+	instance.global_position = global_position
+	instance.emitting = true
+	
+	# Alota alusta
+	set_deferred("global_position", spawn_pos) # global_position = spawn_pos
